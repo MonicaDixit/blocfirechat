a4f8e93ca7f5bb78b7ea9a66515dc6d5c6d0be0c
Monica <mdixit81@gmail.com>
2015-07-30 00:06:33 -0700

    code cleanup

--- app/scripts/roomservice.js
+++ app/scripts/roomservice.js
@@ -1,16 +1,4 @@
 'use strict' ;
-// angular.module('firechatApp')
-//   .factory('Rooms' , ['$firebase' , 'FBURL',  function RoomFactory($firebase, FBURL){
-
-//   var ref = new Firebase(FBURL);
-//   //var rooms = $firebase(ref.child('rooms')).$asArray();
-
-//   return {
-//     all: function(){
-//       return rooms;
-//     }
-//   }
-// }])
 
 angular.module('firechatApp').service('roomService', function($firebase, FBURL) {
 
@@ -31,26 +19,3 @@ angular.module('firechatApp').service('roomService', function($firebase, FBURL) 
         getrooms: getrooms
     };
 })
-
-// myApp.controller('firstController', ['$scope', 'myService',
-//     function firstController($scope, myService) {
-
-//         var setData = function (data) {
-//             myService.setData(data);
-//         }
-
-//     }
-// ]);
-
-// myApp.controller('secondController', ['$scope', 'myService',
-//     function secondController($scope, myService) {
-
-//         myService..registerVisitor(this);
-
-//         this.visit = function () {
-//             $scope.data = myService.getData();
-//         }
-
-//         $scope.data = myService.getData();
-//     }
-// ]);
