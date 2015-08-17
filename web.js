71320693cd98d1f12c53e64f9ed2850f544205eb
Monica <mdixit81@gmail.com>
2015-08-17 09:39:26 -0700

    Add static call to server

--- web.js
+++ web.js
@@ -5,4 +5,5 @@ var app = express();
 app.use(express.logger('dev'));
 var dir = process.cwd() + '/dist';
 app.use(gzippo.staticGzip(dir));
+app.use(express.static(dir));
 app.listen(process.env.PORT || 5000);
